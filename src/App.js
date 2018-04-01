fd4c449451acae57ab4e9e50ae02850cae6561e2
Frances Jurek <jurek.frances@gmail.com>
2018-04-01 14:37:31 -0500

    add text and input

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <h1>Why hello!</h1>
+        <h2>What's your name?</h2>
+        <h2>My name is <input /></h2>
+        <h2>Nice to meet you!</h2>
       </div>
     );
   }
