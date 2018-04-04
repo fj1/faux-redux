be4572490859d0721f450207cefd79ab81d5349c
Frances Jurek <jurek.frances@gmail.com>
2018-04-03 21:19:14 -0500

    move Provider component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 
 import './App.css';
 import Greeting from './Greeting';
+import Provider from './Provider';
 
 class App extends Component {
   render() {
     return (
-      <div className='App'>
-        <Greeting />
-      </div>
+      <Provider>
+        <div className='App'>
+          <Greeting />
+        </div>
+      </Provider>
     );
   }
 }
